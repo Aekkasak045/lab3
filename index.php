501c8757955a5a332ccceeb61ac9a40f27898a1a
Aekkasak_Makhamjan <eqadzc@gmail.com>
2023-01-09 12:11:56 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <form action="show.php">
+        what is you name:<input type="text" name="name">.<br>
+        what is yot age:<input type="text" name="age">.<br>
+        <input type="radio" name="gender" value="female">Female
+        <input type="radio" name="gender" value="male">Male
+        <input type="radio" name="gender" value="other">Other<br>
+        <input type="submit" value="Submit"><br>
+
+    </form>
+
+</body>
+
+</html>
